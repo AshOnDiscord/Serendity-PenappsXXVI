e1407a1b65170edd185e9c208116e3708dd96afb
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2025-09-21 06:29:19 -0400

    e

--- embedding-extraction/recalc.py
+++ embedding-extraction/recalc.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import umap
 import ast
+from collections import Counter
 
-# Load the Parquet files
+# --- Load your Parquet file ---
 df = pd.read_parquet("full_parq.parquet")
+
+df.to_csv("full_parq.csv", index=False)
